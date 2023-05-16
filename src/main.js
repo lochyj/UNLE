2a35d32496fbcf23cab63d0951e325b5f54f3614
Lachlan Jowett <lochyjowett1@gmail.com>
2023-05-16 21:45:42 +1000

    Working on new algorithm for node layout - having issues

--- src/main.js
+++ src/main.js
@@ -62,6 +62,7 @@ class UNLE {
         UNLE.app.stage.on('pointerup', UNLE.onDragEnd);
         UNLE.app.stage.on('pointerupoutside', UNLE.onDragEnd);
 
+        //UNLE.app.stage.scale = {x: 0.25, y: 0.25};
 
         UNLE.LinesContainer = new PIXI.Container();
         UNLE.NodesContainer = new PIXI.Container();
@@ -271,53 +272,109 @@ class UNLE {
         }
     }
     
-    static fruchtermanReingold() {
-        
-        const nodes = UNLE.NodesContainer.children;
-        const edges = UNLE.edgesList;
-
-        // Initialize forces
-        nodes.forEach(node => {
-            node.dx = 0;
-            node.dy = 0;
-        })
-
-        // Calculate repulsive forces between nodes
-        nodes.forEach(node1 => {
-            nodes.forEach(node2 => {
-                if (node1 !== node2) {
-                    const delta_x = node1.x - node2.x;
-                    const delta_y = node1.y - node2.y;
-                    const distance = Math.sqrt(delta_x * delta_x + delta_y * delta_y);
-                    const force = ((UNLE.constant * distance * distance) ** 0.3) / distance; // I guessed this part to be the solution to fixing the physics engine
-                    node1.dx += (delta_x / distance) * force;
-                    node1.dy += (delta_y / distance) * force;
+    static kamadaKawaiAlgorithm(edgesList, nodesEdgeNum, nodesContainer) {
+        const maxIterations = 1000; // Maximum number of iterations
+        const epsilon = 0.1; // Tolerance for convergence
+        const k = 1; // Ideal edge length
+    
+        const n = nodesContainer.children.length; // Number of nodes
+        const L = UNLE.calculateDistanceMatrix(nodesContainer.children); // Calculate initial distance matrix
+    
+        // Initialize positions
+        let positions = [];
+        for (let i = 0; i < n; i++) {
+            positions[i] = { x: 0, y: 0 };
+        }
+    
+        let maxDisplacement = epsilon + 1;
+        let iterations = 0;
+    
+        // Main loop
+        while (maxDisplacement > epsilon && iterations < maxIterations) {
+            let displacement = [];
+    
+            // Calculate pairwise distances and displacements
+            for (let i = 0; i < n; i++) {
+                displacement[i] = { dx: 0, dy: 0 };
+    
+                for (let j = 0; j < n; j++) {
+                    if (i !== j) {
+                        const d = Math.max(0.01, UNLE.calculateEuclideanDistance(positions[i], positions[j]));
+                        const delta = (L[i][j] - d) / d;
+                        const dx = (positions[i].x - positions[j].x) * delta;
+                        const dy = (positions[i].y - positions[j].y) * delta;
+    
+                        displacement[i].dx += dx;
+                        displacement[i].dy += dy;
+                    }
                 }
-            })
-        })
-
-        // Calculate attractive forces along edges
-        edges.forEach(edge => {
-            const source = UNLE.NodesContainer.getChildByName(edge[0]);
-            const target = UNLE.NodesContainer.getChildByName(edge[1]);
-            const delta_x = target.x - source.x;
-            const delta_y = target.y - source.y;
-            const distance = Math.sqrt(delta_x * delta_x + delta_y * delta_y);
-            const force = (distance * distance) / (UNLE.constant * edge[2]);
-            source.dx += (delta_x / distance) * force;
-            source.dy += (delta_y / distance) * force;
-            target.dx -= (delta_x / distance) * force;
-            target.dy -= (delta_y / distance) * force;
-        });
-
-        // Move each node
-        nodes.forEach(node => {
-            //if (Math.abs(node.dx) > 0.001)
-                node.x += node.dx;
-            
-            //if (Math.abs(node.dy) > 0.001)
-                node.y += node.dy;
-        })
+            }
+    
+            // Update positions
+            for (let i = 0; i < n; i++) {
+                positions[i].x += displacement[i].dx;
+                positions[i].y += displacement[i].dy;
+            }
+    
+            // Calculate maximum displacement
+            maxDisplacement = 0;
+            for (let i = 0; i < n; i++) {
+                const displacementMagnitude = UNLE.calculateEuclideanDistance({ x: 0, y: 0 }, displacement[i]);
+    
+                if (displacementMagnitude > maxDisplacement) {
+                    maxDisplacement = displacementMagnitude;
+                }
+            }
+    
+            iterations++;
+        }
+    
+        return positions;
+    }
+    
+    // Helper function to calculate the distance matrix
+    static calculateDistanceMatrix(nodes) {
+        const n = nodes.length;
+        const distanceMatrix = [];
+    
+        for (let i = 0; i < n; i++) {
+            distanceMatrix[i] = [];
+    
+            for (let j = 0; j < n; j++) {
+                distanceMatrix[i][j] = Infinity;
+            }
+    
+            distanceMatrix[i][i] = 0;
+        }
+    
+        // This means that nodeIds are integers and this is most likely not the case... try to fix this in the future.
+        for (const [nodeId1, nodeId2, edgeLength] of UNLE.edgesList) {
+            let i, j;
+            i = nodeId1 - 1;
+            j = nodeId2 - 1;
+    
+            distanceMatrix[i][j] = edgeLength;
+            distanceMatrix[j][i] = edgeLength;
+        }
+    
+        for (let k = 0; k < n; k++) {
+            for (let i = 0; i < n; i++) {
+                for (let j = 0; j < n; j++) {
+                    if (distanceMatrix[i][j] > distanceMatrix[i][k] + distanceMatrix[k][j]) {
+                        distanceMatrix[i][j] = distanceMatrix[i][k] + distanceMatrix[k][j];
+                    }
+                }
+            }
+        }
+    
+        return distanceMatrix;
+    }
+    
+    // Essentially find the distance between two points
+    static calculateEuclideanDistance(point1, point2) {
+        const dx = point2.x - point1.x;
+        const dy = point2.y - point1.y;
+        return Math.sqrt(dx * dx + dy * dy);
     }
 
     static sleep(ms) {
@@ -330,7 +387,12 @@ class UNLE {
 
             if (UNLE.edgesList != []) {
                 UNLE.applyCollisions() // This line is absolutely necessary or nodes made wipe themselves into the shadow realm if they overlap
-                UNLE.fruchtermanReingold();
+                const positions = UNLE.kamadaKawaiAlgorithm(UNLE.edgesList, UNLE.nodesEdgeNum, UNLE.NodesContainer);
+                console.log(positions)
+                for (let i = 0; i < UNLE.NodesContainer.children.length; i++) {
+                    UNLE.NodesContainer.children[i].x = positions[i].x;
+                    UNLE.NodesContainer.children[i].y = positions[i].y;
+                }
             }
             //UNLE.move_largest_node_to_center() 
             UNLE.constrainToBounds();
