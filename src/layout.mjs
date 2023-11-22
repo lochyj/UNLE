55b4d36c7331f50d3df34e2ad75ea9bb74321745
Lachlan Jowett <lachlan@jowett.net.au>
2023-11-22 21:02:37 +1100

    Update layout.mjs

--- src/layout.mjs
+++ src/layout.mjs
@@ -1,12 +1,19 @@
 
+function spring(graph) {
+
+}
+
+function repulsion(graph) {
+    // TODO...
+}
+
+function gravity(graph) {
+    
+}
+
 function layout_engine(graph) {
-
-    for (var i = 0; i < Object.keys(graph.nodes).length; i++) {
-        const key = Object.keys(graph.nodes)[i];
-
-        graph.nodes[key].x = Math.random() * 300;
-        graph.nodes[key].y = Math.random() * 300;
-    }
+    spring(graph);
+    repulsion(graph);
 }
 
 export { layout_engine }
